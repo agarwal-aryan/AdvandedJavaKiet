ec2c4335aba86b6573f3e73ef1b351fd38b7c4a5
aryan <aryan.25161053@kiet.edu>
2026-02-03 00:27:12 +0530

    first java pgm

---
+++ Calculator.java
@@ -0,0 +1,29 @@
+package com;
+
+import java.util.Scanner;
+
+class Sum{
+    Sum(int a, int b){
+        System.out.printf("com.Sum of %d and %d is %d\n", a, b, a+b);
+    }
+}
+class Product{
+    void product(int a, int b){
+        System.out.println("com.Product of "+a+" and "+b +" is "+(a*b) );
+    }
+
+}
+
+class Calculator{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        System.out.println(new Sum(a,b));
+        Product p = new Product();
+        p.product(a,b);
+        System.out.println("hello");
+    }
+}
