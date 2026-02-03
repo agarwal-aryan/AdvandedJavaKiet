699a1c260ac3a9043c2b67e1c11cc2ee97c767d7
aryan <aryan.25161053@kiet.edu>
2026-02-03 20:57:21 +0530

    updated

--- Calculator.java
+++ Calculator.java
@@ -3,8 +3,12 @@ package com;
 import java.util.Scanner;
 
 class Sum{
-    Sum(int a, int b){
-        System.out.printf("com.Sum of %d and %d is %d\n", a, b, a+b);
+//    int a;
+//    Sum(int a, int b){
+//        System.out.printf("com.Sum of %d and %d is %d\n", a, b, a+b);
+//    }
+    void print(){
+        System.out.println("hello");
     }
 }
 class Product{
@@ -21,9 +25,11 @@ class Calculator{
         int a = sc.nextInt();
         System.out.print("Enter second number: ");
         int b = sc.nextInt();
-        System.out.println(new Sum(a,b));
+        System.out.println(new Sum());
+        new Sum().print();
         Product p = new Product();
         p.product(a,b);
         System.out.println("hello");
     }
 }
+
